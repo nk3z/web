d9bfce16a5bc36250ff1eb6746ced3ba2868ccc1
n4k3z <nk3z@proton.me>
2024-06-10 19:18:33 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,96 +1,74 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // Selecciona todos los enlaces de la página que no estén en el menú de navegación
-    const links = document.querySelectorAll('a:not(.main-nav a)');
+    const links = document.querySelectorAll("a:not(.main-nav a)");
 
     // Añade el atributo target="_blank" y rel="noopener noreferrer" a cada enlace seleccionado
-    links.forEach(function(link) {
-        link.setAttribute('target', '_blank');
-        link.setAttribute('rel', 'noopener noreferrer');
+    links.forEach(function (link) {
+        link.setAttribute("target", "_blank");
+        link.setAttribute("rel", "noopener noreferrer");
     });
 
     // Variables y funciones para los sliders de imágenes
-    let currentIndex = 0;
+    const sliders = document.querySelectorAll(".slider");
+    sliders.forEach((slider, index) => {
+        let currentIndex = 0;
+        const slides = slider.querySelector(".slides");
 
-    function prevSlide() {
-        const slides = document.querySelector('.slider .slides');
-        currentIndex = (currentIndex === 0) ? slides.children.length - 1 : currentIndex - 1;
-        updateSlidePosition(slides);
-    }
+        function prevSlide() {
+            currentIndex = currentIndex === 0 ? slides.children.length - 1 : currentIndex - 1;
+            updateSlidePosition();
+        }
 
-    function nextSlide() {
-        const slides = document.querySelector('.slider .slides');
-        currentIndex = (currentIndex === slides.children.length - 1) ? 0 : currentIndex + 1;
-        updateSlidePosition(slides);
-    }
+        function nextSlide() {
+            currentIndex = currentIndex === slides.children.length - 1 ? 0 : currentIndex + 1;
+            updateSlidePosition();
+        }
 
-    function updateSlidePosition(slides) {
-        const slideWidth = slides.children[0].clientWidth;
-        slides.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
-    }
+        function updateSlidePosition() {
+            const slideWidth = slides.children[0].clientWidth;
+            slides.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+        }
 
-    // Event listeners para los botones del slider de imágenes
-    document.querySelectorAll('.slider .prev').forEach(function(button) {
-        button.addEventListener('click', prevSlide);
+        slider.querySelector(".prev").addEventListener("click", prevSlide);
+        slider.querySelector(".next").addEventListener("click", nextSlide);
     });
 
-    document.querySelectorAll('.slider .next').forEach(function(button) {
-        button.addEventListener('click', nextSlide);
-    });
-
-    // Variables y funciones para los sliders de videos
-    const videoPlayers = [
-        document.getElementById('videoPlayer1'),
-        document.getElementById('videoPlayer2'),
-        document.getElementById('videoPlayer3'),
-        document.getElementById('videoPlayer4'),
-        document.getElementById('videoPlayer5')
-    ];
-
+    // Variables y funciones para el único slider de video
+    const videoPlayer = document.getElementById("videoPlayer5");
     const videoSources = [
-        ['videos/tools/video01.mp4', 'videos/tools/video02.mp4', 'videos/tools/video03.mp4', 'videos/tools/video04.mp4', 'videos/tools/video05.mp4', 'videos/tools/video06.webm', 'videos/tools/video07.webm'],
-        ['videos/tools2/video01.mp4', 'videos/tools2/video02.mp4'],
-        ['videos/engines/video01.mp4', 'videos/engines/video02.webm'],
-        ['videos/nodes/video01.mp4', 'videos/nodes/video02.webm'],
-        ['videos/promos/video01.mp4', 'videos/promos/video02.mp4', 'videos/promos/video03.mp4', 'videos/promos/video04.mp4', 'videos/promos/video05.mp4', 'videos/promos/video06.webm']
+        "videos/promos/video01.mp4",
+        "videos/promos/video02.mp4",
+        "videos/promos/video03.mp4",
+        "videos/promos/video04.mp4",
+        "videos/promos/video05.mp4",
+        "videos/promos/video06.webm",
     ];
-    let currentVideoIndexes = Array(videoPlayers.length).fill(0);
+    let currentVideoIndex = 0;
 
-    function changeVideo(index, playerIndex) {
-        const videoPlayer = videoPlayers[playerIndex];
-        videoPlayer.classList.add('fade-out');
+    function changeVideo(index) {
+        videoPlayer.classList.add("fade-out");
 
         setTimeout(() => {
-            currentVideoIndexes[playerIndex] = index;
-            videoPlayer.src = videoSources[playerIndex][currentVideoIndexes[playerIndex]];
+            currentVideoIndex = index;
+            videoPlayer.src = videoSources[currentVideoIndex];
             videoPlayer.load();
             videoPlayer.play();
-            videoPlayer.classList.remove('fade-out');
-            videoPlayer.classList.add('fade-in');
+            videoPlayer.classList.remove("fade-out");
+            videoPlayer.classList.add("fade-in");
         }, 500); // Tiempo que tarda el efecto fade-out
     }
 
-    function prevVideoSlide(playerIndex) {
-        const newIndex = (currentVideoIndexes[playerIndex] === 0) ? videoSources[playerIndex].length - 1 : currentVideoIndexes[playerIndex] - 1;
-        changeVideo(newIndex, playerIndex);
+    function prevVideoSlide() {
+        const newIndex = currentVideoIndex === 0 ? videoSources.length - 1 : currentVideoIndex - 1;
+        changeVideo(newIndex);
     }
 
-    function nextVideoSlide(playerIndex) {
-        const newIndex = (currentVideoIndexes[playerIndex] === videoSources[playerIndex].length - 1) ? 0 : currentVideoIndexes[playerIndex] + 1;
-        changeVideo(newIndex, playerIndex);
+    function nextVideoSlide() {
+        const newIndex = currentVideoIndex === videoSources.length - 1 ? 0 : currentVideoIndex + 1;
+        changeVideo(newIndex);
     }
 
-    // Event listeners para los botones del slider de videos
-    document.querySelectorAll('.video-slider-container .prev').forEach(function(button) {
-        button.addEventListener('click', function() {
-            const playerIndex = button.getAttribute('data-index') - 1;
-            prevVideoSlide(playerIndex);
-        });
-    });
-
-    document.querySelectorAll('.video-slider-container .next').forEach(function(button) {
-        button.addEventListener('click', function() {
-            const playerIndex = button.getAttribute('data-index') - 1;
-            nextVideoSlide(playerIndex);
-        });
-    });
+    // Event listeners para los botones del slider de video
+    document.querySelector(".video-slider-container .prev").addEventListener("click", prevVideoSlide);
+    document.querySelector(".video-slider-container .next").addEventListener("click", nextVideoSlide);
 });
